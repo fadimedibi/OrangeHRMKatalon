742ff6bd965c99a6d49df4a06b900d441505c158
fadimedibi <fadimedibi@gmail.com>
2020-10-26 17:03:46 -0500

    Orange HRM project is added.

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7bdcb79b-f822-4468-999b-90de96b29b8d</testSuiteGuid>
+   <testCaseLink>
+      <guid>8be47642-5bbd-46d6-b293-4360f9b5f1c4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_1</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>fdaf4758-b767-49b8-abb6-bd58b9df6958</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>32de1263-d3e1-4838-9040-42cc3f9c2b68</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e02a59eb-2b37-4174-b030-da7ba0d26507</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Test_2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>65e59bbf-d163-4bed-b1cc-20789d9763ed</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Test_3</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/TestSuite2.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>05e28aec-fdfd-4832-ad5b-734d396d792e</testSuiteGuid>
+   <testCaseLink>
+      <guid>cb3647ca-702d-48a5-bce9-03c5f5135354</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_3</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
